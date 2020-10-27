5f0683775126a5beb49c0c56cc58acb271d157f6
Pierrick Gaudin <pierrick.gaudin@student.ecp.fr>
2020-10-27 17:37:02 +0000

    savage parrot

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,5 +1,5 @@
 exports.handler = async function(event, context) {
-    if (event.queryStringParameters.message !== "") {
+    if (event.queryStringParameters.message) {
         return {
             statusCode: 200,
             body: JSON.stringify(event.queryStringParameters.message)
